c3b6d39b07f5e954b0766fb8c2fc3e4a5404d9b8
Bia Chacon <anabheatrizchacon@outlook.com>
2019-08-27 22:27:58 -0300

    .

--- src/ag/Principal.java
+++ src/ag/Principal.java
@@ -4,6 +4,12 @@ package ag;
 public class Principal {
 
     // Knapsack related variables
+    public static int nuItens ;
+    public static double[] valor ;
+    public static double[] peso;
+    public static double knapsackMaxSize;
+
+    /*
     public static final int nuItens = 10;
     public static final double[] valor = {1,2,10,2,69,5,5,8,1,1};
     public static final double[] peso = {1,2,4,1,1,6,8,2,9,10};
@@ -13,5 +19,5 @@ public class Principal {
         Genetico ag = new Genetico();
         ag.executarAG();
     }
-
+    */
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,58 @@
 package sample;
 
+import ag.Genetico;
+import ag.Principal;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import pso.Nuvem;
+
 public class Controller {
+
+    @FXML
+    private Button btAG, btPSO;
+
+    @FXML
+    private TextField textPeso;
+
+    @FXML
+    private TextField textValor;
+
+    @FXML
+    private TextField textKnapsackMaxSize;
+
+    @FXML
+    private void executarAG() {
+
+        double knapsackMaxSize = Double.parseDouble(textKnapsackMaxSize.getText());
+        Principal.knapsackMaxSize = knapsackMaxSize;
+
+        String[] arrayPeso = textPeso.getText().split(" ");
+
+        System.out.println(arrayPeso.length);
+        System.out.println(Principal.knapsackMaxSize);
+
+        double[] peso = {};
+        for (int i = 0 ; i < arrayPeso.length; i++){
+            peso[i] = Double.parseDouble(arrayPeso[i]);
+        }
+        Principal.peso = peso;
+
+        String[] arrayValor = textValor.getText().split(" ");
+        double[] valor = {};
+        for (int i = 0 ; i < arrayValor.length; i++){
+            valor[i] = Double.parseDouble(arrayValor[i]);
+        }
+        Principal.valor = valor;
+
+        Principal.nuItens = peso.length;
+
+        Genetico ag = new Genetico();
+        ag.executarAG();
+
+    }
+
+    @FXML
+    private void executarPSO() {}
+
 }
